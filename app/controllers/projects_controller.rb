3620d0a5c99baafd7805531322e369176bc0f63a
link-coala <omj530@gmail.com>
2018-03-01 07:04:15 -0500

    Projects can now be updated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,9 +5,24 @@ end
 def show
 @project = Project.find(params[:id])
 end
+def edit
+@project = Project.find(params[:id])
+end
 def new
 @project = Project.new
 end
+
+def update
+@project = Project.find(params[:id])
+if @project.update(project_params)
+flash[:notice] = "Project has been updated."
+redirect_to @project
+else
+flash.now[:alert] = "Project has not been updated."
+render "edit"
+end
+end
+
 def create
 @project = Project.new(project_params)
 if @project.save
@@ -17,6 +32,7 @@ else
 flash.now[:alert] = "Project has not been created."
 render "new"
 end
+
 end
 private
 def project_params
