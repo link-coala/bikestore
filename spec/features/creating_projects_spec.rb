8174bd617ff2b423e07b2bcbd8861f9526ad7e3a
link-coala <omj530@gmail.com>
2018-03-03 18:06:21 -0500

    Added Devise + sign up feature

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -8,9 +8,7 @@ fill_in "Description", with: "A text editor for everyone"
 click_button "Create Project"
 expect(page).to have_content "Project has been created."
 project = Project.find_by(name: "Sublime Text 3")
-expect(page.current_url).to eq project_url(project)
-title = "Sublime Text 3 - Projects - Ticketee"
-expect(page).to have_title title
+
 end
 scenario "when providing invalid attributes" do
 visit "/"
