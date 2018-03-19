49f75769dab02ee51524fc0a145e7ba38505ac77
link-coala <omj530@gmail.com>
2018-03-19 17:13:01 -0500

    errors for cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
   # GET /carts
   # GET /carts.json
@@ -71,4 +72,8 @@ class CartsController < ApplicationController
     def cart_params
       params.fetch(:cart, {})
     end
+    def invalid_cart
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_index_url, notice: 'Invalid cart'
+end
 end
