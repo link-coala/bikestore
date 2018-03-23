a278f8a635248a736e6ee9eb80ad4c885bcfad8f
link-coala <omj530@gmail.com>
2018-03-23 17:27:33 -0500

    first_check_check_out_page_176

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319121921) do
+ActiveRecord::Schema.define(version: 20180323215538) do
 
   create_table "bikes", force: :cascade do |t|
     t.string "model"
@@ -33,8 +33,19 @@ ActiveRecord::Schema.define(version: 20180319121921) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
+    t.integer "order_id"
     t.index ["bike_id"], name: "index_line_items_on_bike_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["order_id"], name: "index_line_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "email"
+    t.integer "pay_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
