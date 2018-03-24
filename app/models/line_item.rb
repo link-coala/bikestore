efcc336893215785d2197faceb652703af1d44ef
link-coala <omj530@gmail.com>
2018-03-24 03:14:33 -0500

    sending_orders_no_ajax

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,6 @@
 class LineItem < ApplicationRecord
-  belongs_to :bike
+  belongs_to :order, optional: true
+  belongs_to :bike, optional: true
   belongs_to :cart
 def total_price
 bike.price * quantity
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,14 @@
 class Order < ApplicationRecord
+has_many :line_items, dependent: :destroy
 enum pay_type: {
 "Check" => 0,
 "Credit card" => 1,
 "Purchase order" => 2
 }
+def add_line_items_from_cart(cart)
+cart.line_items.each do |item|
+item.cart_id = nil
+line_items << item
+end
+end
 end
